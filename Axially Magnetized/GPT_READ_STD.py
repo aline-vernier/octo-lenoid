84ebaa1602c00cd282b44428b5bc8c53985daf5e
Aline Vernier <aline.vernier@ensta.fr>
2018-03-21 11:12:43 +0100

    STD file loading OK

--- Axially Magnetized/GPT_READ_STD.py
+++ Axially Magnetized/GPT_READ_STD.py
@@ -15,7 +15,6 @@ from scipy.constants import c
 from scipy.interpolate import UnivariateSpline as UnivariateSpline
 import re
 
-
 ###################################################################
 #                IMPORT FILE GENERATED FROM GDFA                  #
 ###################################################################
@@ -27,11 +26,34 @@ w_file = "\\std_SalleNoire_beam_h.txt"
 file_data = []
 with open(w_dir + w_file) as my_file:
     for line in my_file:
-        this_line = line.split()
-        file_data.append(this_line)
+        file_data.append(line.split())
 
-header = file_data[0:7]
-data = file_data[8::]
+# print file_data[0:7]
 
-print header
+multirun = bool(file_data[0][1])
+energy_scan = bool(file_data[1][1])
+n_e_steps = float(file_data[2][1])
+l_map_scan = bool(file_data[3][1])
+n_lmap_steps = float(file_data[4][1])
+group_by = file_data[5][1]
+n_t_steps = int(file_data[6][1])
 
+file_data = file_data[8::]
+data = []
+data_temp = []
+l_map_vals = []
+labels = []
+
+if multirun:
+    idx_2_max = n_t_steps + 3
+    labels = file_data[1]
+    for idx_1 in range(len(file_data) % (n_t_steps + 3)):
+        l_map_vals.append(float(file_data[idx_1*(idx_2_max + 1)][1]))
+        for idx_2 in range(2, idx_2_max):
+            this_element = map(float,
+                               file_data[idx_1*(idx_2_max + 1) + idx_2])
+            data_temp.append(this_element)
+            data.append(data_temp)
+
+data = np.array(data)
+print labels
